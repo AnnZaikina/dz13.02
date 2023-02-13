9b10d9a2e65142b27c76aa55fb96feb30ccfb51a
AnnaZ <annazaikina271@gmail.com>
2023-02-13 21:19:34 +0300

    Чётное или нечётное число

---
+++ task6evennumb/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine()?? "0");
+if(number1 % 2 == 0)
+  {
+        Console.WriteLine("Число " + number1 + " чётное");
+    }
+  else
+  {
+        Console.WriteLine("Число " + number1 + " нечётное");
+    }
